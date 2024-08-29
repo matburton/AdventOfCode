2b7e6b5794d89ec890871b9e5c3e136b0aea49ef
matburton <mat.burton@red-gate.com>
2024-08-29 01:43:01 +0100

    AoC: Day 12 - Part 2
    Which meant a complete rethink and rewrite.
    
    This still doesn't pass in a reasonable time frame?
    Not sure if the answer is correct?

--- src/day_12.rs
+++ src/day_12.rs
@@ -7,93 +7,50 @@ const EXAMPLE: &str = "???.### 1,1,3\n\
                        ????.#...#... 4,1,1\n\
                        ????.######..#####. 1,6,5\n\
                        ?###???????? 3,2,1";
+
+fn parse_spec(text: &str) -> Vec<usize> {
+    text.split(',').map(|f| f.parse().unwrap()).collect()
+}
+
+fn arrangements(mut row: &str, spec: &[usize]) -> usize {
+
+    let left = spec.iter().sum::<usize>();
+
+    if row.chars().filter(|&c| c == '#').count() > left { return 0; }
+
+    let run_length = match spec { [l, ..] => *l, [] => { return 1; } };
+
+    let mut count = 0;
+
+    while row.len() >= left + spec.len() - 1
+        && row.chars().filter(|&c| c != '.').count() >= left {
+
+        let fits = !row[.. run_length].contains('.')
+                 && row.chars().nth(run_length) != Some('#');
+
+        if fits {
+
+            let row = if row.len() > run_length { &row[run_length + 1 ..] }
+                                           else { Default::default() };
+
+            count += arrangements(row, &spec[1 ..]);
+        }
+
+        row = if &row[.. 1] == "#" { break; } else { &row[1 ..] };
+    }
+
+    count
+}
+
 mod part_1 {
 
     use super::*;
 
-    use std::iter::repeat;
-
-    #[derive(PartialEq, Eq, Clone, Copy)]
-    enum Spring { Operational, Damaged, Unknown }
-
-    use Spring::*;
-
-    struct Config<'a> { springs: &'a [Spring],
-                        spec:    &'a [usize],
-                        spacing: &'a mut [usize] }
-
-    fn possible(config: &Config) -> bool {
-
-        let base_space = |i|
-            if i == 0 || i == config.spacing.len() { 0 } else { 1 };
-
-        config.spacing
-              .iter()
-              .cloned()
-              .enumerate()
-              .map(|(i, v)| v + base_space(i))
-              .zip(config.spec.iter().cloned().chain(repeat(0).take(1)))
-              .map(|(s, d)| (repeat(Operational).take(s),
-                             repeat(Damaged).take(d)))
-              .flat_map(|(o, d)| o.chain(d))
-              .zip(config.springs.iter().cloned())
-              .all(|(a, b)| a == b || b == Unknown)
-    }
-
-    fn recurse(config: &mut Config, index: usize, left: usize) -> usize {
-
-        if index == config.spacing.len() - 1 {
-
-            config.spacing[index] = left;
-
-            return match possible(config) { true => 1, _ => 0 };
-        }
-
-        let mut count = 0;
-
-        for value in 0 ..=left {
-
-            config.spacing[index] = value;
-
-            count += recurse(config, index + 1, left - value);
-        }
-
-        count
-    }
-
-    fn arrangements(springs: &[Spring], spec: &[usize]) -> usize {
-        
-        let mut spacing =
-            Vec::from_iter(repeat::<usize>(0).take(spec.len() + 1));
-
-        let left = springs.len()
-                 - spec.iter().sum::<usize>()
-                 + 1
-                 - spec.len();
-
-        let mut config = Config { springs, spec, spacing: &mut spacing };
-
-        recurse(&mut config, 0, left)
-    }
-
     fn get_result(input: &str) -> usize {
-
-        let parse_spring = |c| match c { '.' => Operational,
-                                         '#' => Damaged,
-                                         '?' => Unknown,
-                                         _   => panic!() };
-
-        let parse_springs = |text: &str| -> Vec<_> {
-            text.chars().map(parse_spring).collect()
-        };
-
-        let parse_spec = |text: &str| -> Vec<_> {
-            text.split(',').map(|f| f.parse().unwrap()).collect()
-        };
 
         input.split('\n')
              .map(|l| l.split(' ').collect::<Vec<_>>())
-             .map(|v| arrangements(&parse_springs(v[0]), &parse_spec(v[1])))
+             .map(|v| arrangements(v[0], &parse_spec(v[1])))
              .sum()
     }
 
@@ -102,4 +59,34 @@ mod part_1 {
     
     #[test]
     fn real() { assert_eq!(get_result(INPUT), 7007); }
+}
+
+mod part_2 {
+
+    use super::*;
+
+    use std::{ iter::repeat, io::stdout, io::Write };
+
+    fn get_result(input: &str) -> usize {
+
+        let unfold = |t, s| repeat(t).take(5).collect::<Vec<_>>().join(s);
+
+        input.split('\n')
+             .enumerate()
+             .inspect(|(i, l)| { print!("{:<4} {}", i, &l);
+                                 let _ = stdout().flush(); })
+             .map(|(_, l)| l.split(' ').collect::<Vec<_>>())
+             .map(|v| (unfold(v[0], "?"), unfold(v[1], ",")))
+             .map(|t| (std::time::Instant::now(),
+                       arrangements(&t.0, &parse_spec(&t.1))))
+             .inspect(|(t, c)| println!(" -> {} ({:?})", c, t.elapsed()))
+             .map(|(_, c)| c)
+             .sum()
+    }
+
+    #[test]
+    fn example() { assert_eq!(get_result(EXAMPLE), 525152); }
+    
+    #[test]
+    fn real() { assert_eq!(get_result(INPUT), 0); }
 }
