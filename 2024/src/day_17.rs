728389112e0a2347307901d5231a7402b3800ca1
matburton <mat.burton@red-gate.com>
2024-12-18 00:14:36 +0000

    2024: Day 17 - Part 1
    That was a bit different and fun

--- 2024/src/day_17.rs
+++ 2024/src/day_17.rs
@@ -7,27 +7,67 @@ const EXAMPLE: &str = "Register A: 729\n\
                        \n\
                        Program: 0,1,5,4,3,0";
 
-struct Computer { program_counter: usize,
-                  registers: [isize; 3],
+struct Computer { counter: usize,
+                  registers: [usize; 3],
                   program: Vec<usize>,
-                  output: Vec<isize> }
-
+                  output: Vec<usize> }
 impl Computer {
 
     fn parse(text: &str) -> Self {
 
-        let sections = text.split("\n\n");
+        let mut sections = text.split("\n\n");
 
-        let registers = sections.next()
-                                .unwrap()
-                                .split('\n')
-                                .map(|l| l.split(' ').last().unwrap())
-                                .map(|f| f.parse::<isize>().unwrap());
+        let registers: [_; 3] = sections.next()
+                                        .unwrap()
+                                        .split('\n')
+                                        .map(|l| l.split(' ').last().unwrap())
+                                        .map(|f| f.parse().unwrap())
+                                        .collect::<Vec<_>>()
+                                        .try_into()
+                                        .unwrap();
+        let program = sections.next()
+                              .unwrap()
+                              .split(' ')
+                              .nth(1)
+                              .unwrap()
+                              .split(',')
+                              .map(|f| f.parse().unwrap())
+                              .collect();
+
+        Self { counter: 0, registers, program, output: Vec::new() }
     }
 
-    fn next(&mut self) -> bool {
+    fn run(&mut self) {
 
-        false
+        while self.counter < self.program.len() - 1 {
+
+            let [code, op] = [self.program[self.counter],
+                              self.program[self.counter + 1]];
+
+            self.counter += 2;
+
+            let combo = match op { 4 .. 7 => self.registers[op - 4], _ => op };
+
+            let mut dv = |r|
+                self.registers[r] = self.registers[0] / (1 << combo);
+
+            match code {
+
+                0 /* adv */ => dv(0),
+            
+                1 /* bxl */ => self.registers[1] ^= op,
+
+                2 /* bst */ => self.registers[1] = combo % 8,
+
+                3 /* jnz */ => if self.registers[0] != 0 { self.counter = op; },
+            
+                4 /* bxc */ => self.registers[1] ^= self.registers[2],
+
+                5 /* out */ => self.output.push(combo % 8),
+
+                c /* bdv, cdv */ => dv(c - 5)
+            };
+        }
     }
 }
 
@@ -35,15 +75,19 @@ mod part_1 {
 
     use super::*;
 
-    fn get_result(input: &str) -> Vec<usize> {
+    fn get_result(input: &str) -> String {
 
-        Vec::new()
+        let mut computer = Computer::parse(input);
+
+        computer.run();
+
+        format!("{:?}", computer.output).replace(['[', ']', ' '], "")
     }
    
     #[test]
-    fn example() { assert_eq!(get_result(EXAMPLE), [4,6,3,5,6,3,5,2,1,0]); }
+    fn example() { assert_eq!(get_result(EXAMPLE), "4,6,3,5,6,3,5,2,1,0"); }
 
    
     #[test]
-    fn real() { assert_eq!(get_result(INPUT), []); }
+    fn real() { assert_eq!(get_result(INPUT), "4,3,7,1,5,3,0,5,4"); }
 }
